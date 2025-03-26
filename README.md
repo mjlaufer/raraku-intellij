6259d77c5bcf9fa8c4ff158c7037c665e4e10132
Matthew Laufer <matthew.j.laufer@gmail.com>
2025-03-25 20:47:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Raraku
+
+Raraku is a dark theme for JetBrains IDEs.
+
+This is a port of [mjlaufer/raraku.nvim](https://www.github.com/mjlaufer/raraku.nvim).
+
+## Installation
+
+1. Clone this repository.
+2. In your JetBrains IDE, click the "Run Plugin" configuration. This will build the plugin at ./build/libs/raraku-intellij-0.0.1.jar.
+3. Go to **Settings > Plugins**, and click the gear icon. Then click **Install Plugin from Disk**, and select the plugin JAR file.
---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    id("java") // Java support
+    id("org.jetbrains.kotlin.jvm") version "1.9.25" // Kotlin support
+    id("org.jetbrains.intellij") version "1.17.4" // Gradle IntelliJ Plugin
+}
+
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+// Configure project's dependencies
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    properties("javaVersion").let {
+        withType<JavaCompile> {
+            sourceCompatibility = it
+            targetCompatibility = it
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = it
+        }
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion")
+    }
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+}
+
+tasks.buildSearchableOptions {
+    enabled = false
+}
